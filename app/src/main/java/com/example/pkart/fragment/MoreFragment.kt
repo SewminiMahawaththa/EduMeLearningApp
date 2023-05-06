de8249ed3b8256e1df718002ec75730d8b21d292
it21308598 <it21308598@my.sliit.lk>
2023-05-06 19:20:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pkart/fragment/MoreFragment.kt
@@ -0,0 +1,36 @@
+package com.example.pkart.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import com.example.pkart.R
+import com.example.pkart.activity.BankCardActivity
+
+
+class MoreFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_more, container, false)
+
+
+
+        return view
+
+
+
+    }
+
+
+}
