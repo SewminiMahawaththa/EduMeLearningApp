e773447a5d4a3528e3ed81ab1339ce521335978a
it21309106 <it21309106@my.sliit.lk>
2023-05-08 23:04:17 +0530

    Create help desk and do some modifications of the layouts

---
+++ app/src/main/java/com/example/pkart/activity/FeedBackActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pkart.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pkart.R
+
+class FeedBackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed_back)
+    }
+}
